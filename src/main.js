19169d0d529db32d5b51c57a664a4b3332b15262
aelvanna <lmarie.fernandez@gmail.com>
2025-08-01 18:17:15 +1000

    Major changes: Improved accessibility and updated validateEmail function. - Added aria-describedby and aria-invalid attributes to input field for accessibility. - Added id to error message paragraph to link error message with input field - Added rel="noreferrer noopener" to footer link for safety and security purposes - Updated validateEmail function to toggle aria-invalid between true and false, depending on the results of the validation - Updated comments in main.js

--- src/main.js
+++ src/main.js
@@ -6,16 +6,22 @@ const dismissMsgButton = document.querySelector(".success-message__button");
 const emailInput = form.querySelector("#form__email");
 const errorMessageText = form.querySelector(".form__error-message");
 
+/** Function
 /* Handles Validation. Checks if an email address has been entered by the user and
 /* if so, whether it is a valid email with characters on either side of the @ symbol.
 /* If either of the above rules are not true, then display an error message.
-/* @param {email} The email address submitted through the form
+/* For users with assistive technology, sets aria-invalid attribute to true if an error
+/* occurs.
+/* @param {email} : The email address submitted through the form
 **/
+
 function validateEmail(email) {
   const isValidEmail = /^\S+@\S+$/g;
   if (!isValidEmail.test(email)) {
+    emailInput.setAttribute("aria-invalid", "true");
     return "Valid email required";
   } else {
+    emailInput.setAttribute("aria-invalid", "false");
     return "";
   }
 }
@@ -23,8 +29,9 @@ function validateEmail(email) {
 /** Function
  * Handles the form submission, displays the success message,
  * and populates it with the submitted email address.
- * @param {Event} e The form submission event.
+ * @param {Event} e : The form submission event.
  **/
+
 function handleSubmit(e) {
   e.preventDefault();
   const data = Object.fromEntries(new FormData(e.target));
@@ -33,6 +40,7 @@ function handleSubmit(e) {
 
   if (!errorMessage) {
     document.body.classList.add("success-state-active");
+
     submittedEmail.innerHTML = data.email;
     // Clear previous error state on success
     emailInput.classList.remove("form__field--error");
@@ -46,18 +54,20 @@ function handleSubmit(e) {
 /** Function
  * Dismisses the success message and returns to the form view.
  */
+
 function dismissSuccessMessage() {
-  // Remove the success state class to switch back to the form view
+  /* Remove the success state class to switch back to the form view */
   document.body.classList.remove("success-state-active");
-  // Reset the form for the next submission
+
+  /* Reset the form for the next submission */
   emailInput.classList.remove("form__field--error");
   errorMessageText.innerHTML = "";
   form.reset();
 }
 
-// Add an event listener to the form__button to handle form submission
+/* Add an event listener to the form__button to handle form submission */
 form.addEventListener("submit", handleSubmit);
 
-// Add an event listener to the success-message__button to remove the success message
-// and display the header and form again.
+/* Add an event listener to the success-message__button to remove the success message */
+/* and display the header and form again. */
 dismissMsgButton.addEventListener("click", dismissSuccessMessage);
